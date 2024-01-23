6bf1f8d427053bbd16f237f6e86e21972ce3f55f
Kim Robinson <kimmykokonut@gmail.com>
2024-01-22 20:05:07 -0800

    list, redirect, razor loops and conditionals

--- ToDoList/Controllers/HomeController.cs
+++ ToDoList/Controllers/HomeController.cs
@@ -1,26 +1,29 @@
 using Microsoft.AspNetCore.Mvc;
 using ToDoList.Models;
+using System.Collections.Generic;
 
 namespace ToDoList.Controllers
 {
   public class HomeController : Controller
   {
-    [Route("/")]
+    [HttpGet("/")]
     public ActionResult Index()
     {
-      Item starterItem = new Item("Add first item to To Do List");
-      return View(starterItem);
+      List<Item> allItems = Item.GetAll();
+      return View(allItems); //shows list
+      // Item starterItem = new Item("Add first item to To Do List");
+      // return View(starterItem); //to show 1 item
     }
-    [Route("/items/new")]
+    [HttpGet("/items/new")]
     public ActionResult CreateForm()
     {
       return View();
     }
-    [Route("/items")]
+    [HttpPost("/items")]
     public ActionResult Create(string description)
     {
       Item myItem = new Item(description);
-      return View("Index", myItem);
+      return RedirectToAction("Index");
     }
   }
 }
