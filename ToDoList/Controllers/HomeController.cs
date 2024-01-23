4dc0ed0b99c78d692ffa27fc6f28a6a3ce9fb52c
Kim Robinson <kimmykokonut@gmail.com>
2024-01-22 20:19:06 -0800

    add second controller

--- ToDoList/Controllers/HomeController.cs
+++ ToDoList/Controllers/HomeController.cs
@@ -1,6 +1,4 @@
 using Microsoft.AspNetCore.Mvc;
-using ToDoList.Models;
-using System.Collections.Generic;
 
 namespace ToDoList.Controllers
 {
@@ -9,21 +7,7 @@ namespace ToDoList.Controllers
     [HttpGet("/")]
     public ActionResult Index()
     {
-      List<Item> allItems = Item.GetAll();
-      return View(allItems); //shows list
-      // Item starterItem = new Item("Add first item to To Do List");
-      // return View(starterItem); //to show 1 item
-    }
-    [HttpGet("/items/new")]
-    public ActionResult CreateForm()
-    {
       return View();
-    }
-    [HttpPost("/items")]
-    public ActionResult Create(string description)
-    {
-      Item myItem = new Item(description);
-      return RedirectToAction("Index");
     }
   }
 }
