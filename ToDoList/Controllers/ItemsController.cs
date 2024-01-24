98dde75cf17adff86fb3e57987e5be1db3cc0786
Kim Robinson <kimmykokonut@gmail.com>
2024-01-23 18:44:03 -0800

    add delete list functionality

--- ToDoList/Controllers/ItemsController.cs
+++ ToDoList/Controllers/ItemsController.cs
@@ -23,5 +23,11 @@ namespace ToDoList.Controllers
       Item myItem = new Item(description);
       return RedirectToAction("Index");
     }
+    [HttpPost("/items/delete")]
+    public ActionResult DeleteAll()
+    {
+      Item.ClearAll();
+      return View();
+    }
   }
 }
