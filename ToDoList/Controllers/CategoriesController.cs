2a32ec84a48c013cf0005c8e7848b72c212e77f8
Kim Robinson <kimmykokonut@gmail.com>
2024-01-24 21:06:28 -0800

    show route in controller and create view show to display category details and links

--- ToDoList/Controllers/CategoriesController.cs
+++ ToDoList/Controllers/CategoriesController.cs
@@ -24,5 +24,15 @@ namespace ToDoList.Controllers
       Category newCategory = new Category(categoryName);
       return RedirectToAction("Index");
     }
+    [HttpGet("/categories/{id}")]
+    public ActionResult Show(int id)
+    {
+      Dictionary<string, object> model = new Dictionary<string, object>();
+      Category selectedCategory = Category.Find(id);
+      List<Item> categoryItems = selectedCategory.Items;
+      model.Add("category", selectedCategory);
+      model.Add("items", categoryItems);
+      return View(model);
+    }
   }
 }
