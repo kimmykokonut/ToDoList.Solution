aefcd199bf7db2f60fe5cec44d98f3bcc5e53d10
Kim Robinson <kimmykokonut@gmail.com>
2024-01-24 21:49:13 -0800

    refactor: remove Item Index action adn view, remove DeleteAll action and view, update item/show route and view

--- ToDoList/Controllers/ItemsController.cs
+++ ToDoList/Controllers/ItemsController.cs
@@ -6,12 +6,6 @@ namespace ToDoList.Controllers
 {
   public class ItemsController : Controller
   {
-    [HttpGet("/items")]
-    public ActionResult Index()
-    {
-      List<Item> allItems = Item.GetAll();
-      return View(allItems); 
-    }
     [HttpGet("/items/new")]
     public ActionResult New()
     {
@@ -21,19 +15,18 @@ namespace ToDoList.Controllers
     public ActionResult Create(string description)
     {
       Item myItem = new Item(description);
-      return RedirectToAction("Index");
+      return RedirectToAction("Index"); //
     }
-    [HttpPost("/items/delete")]
-    public ActionResult DeleteAll()
+    
+    [HttpGet("/categories/{categoryId}/items/{itemId}")]
+    public ActionResult Show(int categoryId, int itemId)
     {
-      Item.ClearAll();
-      return View();
-    }
-    [HttpGet("/items/{id}")]
-    public ActionResult Show(int id)
-    {
-      Item foundItem = Item.Find(id);
-      return View(foundItem);
+      Item item = Item.Find(itemId);
+      Category category = Category.Find(categoryId);
+      Dictionary<string, object> model = new Dictionary<string, object>();
+      model.Add("item", item);
+      model.Add("category", category);
+      return View(model); //passed cat&item id to view in dictionary
     }
   }
 }
