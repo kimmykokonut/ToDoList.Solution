68dadc19614c5d512054990da1e29d3ced1866d3
Kim Robinson <kimmykokonut@gmail.com>
2024-01-24 20:49:39 -0800

    add Index route and view, add new route and view, create route for form to direct to Index view

---
+++ ToDoList/Controllers/CategoriesController.cs
@@ -0,0 +1,28 @@
+using System.Collections.Generic;
+using System;
+using Microsoft.AspNetCore.Mvc;
+using ToDoList.Models;
+
+namespace ToDoList.Controllers
+{
+  public class CategoriesController : Controller
+  {
+    [HttpGet("/categories")]
+    public ActionResult Index()
+    {
+      List<Category> allCategories = Category.GetAll();
+      return View(allCategories);
+    }
+    [HttpGet("/categories/new")]
+    public ActionResult New()
+    {
+      return View();
+    }
+    [HttpPost("/categories")]
+    public ActionResult Create(string categoryName)
+    {
+      Category newCategory = new Category(categoryName);
+      return RedirectToAction("Index");
+    }
+  }
+}
