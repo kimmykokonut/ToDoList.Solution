26ab8975ef649f6ae6c3822093a18c949979bee8
Kim Robinson <kimmykokonut@gmail.com>
2024-01-27 15:57:23 -0800

    rewrite getall to use db, remove _instances, add overloaded constructor for db id

--- ToDoList/Models/Item.cs
+++ ToDoList/Models/Item.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using MySqlConnector;
 
 namespace ToDoList.Models
 {
@@ -6,25 +7,50 @@ namespace ToDoList.Models
   {
     public string Description { get; set; }
     public int Id { get; } //don't need set b/c set automatically in constructor
-    private static List<Item> _instances = new List<Item> { };
 
     public Item(string description)
     {
       Description = description;
-      _instances.Add(this);
-      Id = _instances.Count;
+    }
+    public Item(string description, int id)
+    {
+      Description = description;
+      Id = id;
     }
     public static List<Item> GetAll()
     {
-      return _instances;
+      List<Item> allItems = new List<Item> { };
+      //open new db connection
+      MySqlConnection conn = new MySqlConnection(DBConfiguration.ConnectionString); 
+      conn.Open();
+      //construct SQL query stored in obj MySqlCommand. the 'as' casts cmd into msc obj.
+      MySqlCommand cmd = conn.CreateCommand() as MySqlCommand;
+      //add actual text of sql command, store in CommandText property
+      cmd.CommandText = "SELECT * FROM items;";
+      //create data reader object responsible for reading data returned from db. cast type w/'as'
+      MySqlDataReader rdr = cmd.ExecuteReader() as MySqlDataReader;
+      while (rdr.Read()) //built in Read(). returns boolean. //in loop take ea record from db and translate that record into an Item obj
+      {   
+        int itemId = rdr.GetInt32(0); //0 index of array. id column at index 0, desc@index 1
+        string itemDescription = rdr.GetString(1);
+        Item newItem = new Item(itemDescription, itemId);
+        allItems.Add(newItem);
+      }
+      conn.Close();
+      if (conn != null)
+      {
+        conn.Dispose();
+      }
+      return allItems;
     }
     public static void ClearAll()
     {
-      _instances.Clear();
+      
     }
     public static Item Find(int searchId)
     {
-      return _instances[searchId-1]; // -1 b/c index location != reality
+      Item placeholderItem = new Item("placeholder item");
+      return placeholderItem;
     }
     
   }
