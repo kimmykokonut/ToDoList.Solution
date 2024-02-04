715b062abcd6cf1cf321cb746703a81083453037
Kim Robinson <kimmykokonut@gmail.com>
2024-02-04 15:00:48 -0800

    add create funcitonality for add tag to item

--- ToDoList/Controllers/TagsController.cs
+++ ToDoList/Controllers/TagsController.cs
@@ -37,4 +37,16 @@ public class TagsController : Controller
     _db.SaveChanges();
     return RedirectToAction("Index");
   }
+  public ActionResult AddItem(int id)
+  {
+    Tag thisTag = _db.Tags.FirstOrDefault(tags => tags.TagId == id);
+    ViewBag.ItemId = new SelectList(_db.Items, "ItemId", "Description");
+    return View(thisTag);
+  }
+  [HttpPost]
+  public ActionResult AddItem(Tag tag, int itemId)
+  {
+    #nullable enable
+    ItemTag? joinEntity = _db.ItemTags.FirstOrDefault(join => (join.ItemId == itemId && join.TagId == tag.TagId));
+  }
 }
