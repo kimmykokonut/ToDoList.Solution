e42b6cf3d1b00089f64d1b44ab6e85c549917c1c
Kim Robinson <kimmykokonut@gmail.com>
2024-02-05 16:48:20 -0800

    add validation attribute for item.description

--- ToDoList/Models/Item.cs
+++ ToDoList/Models/Item.cs
@@ -6,6 +6,7 @@ namespace ToDoList.Models
   public class Item
   {
     public int ItemId { get; set; }
+    [Required(ErrorMessage = "The item's description can't be empty!")]
     public string Description { get; set; }
     [Range(1, int.MaxValue, ErrorMessage = "You must add your item to a category. Have you created a category yet?")]
     public int CategoryId { get; set; } //foregin key in items db
