05f24eff7375948fdb10b470ec1a9c7e65accaaa
Kim Robinson <kimmykokonut@gmail.com>
2024-02-06 19:59:14 -0800

    add data from multiple db tables to splash page

--- ToDoList/Controllers/HomeController.cs
+++ ToDoList/Controllers/HomeController.cs
@@ -1,13 +1,33 @@
 using Microsoft.AspNetCore.Mvc;
+using ToDoList.Models;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace ToDoList.Controllers
 {
   public class HomeController : Controller
   {
+    private readonly ToDoListContext _db;
+    public HomeController(ToDoListContext db)
+    {
+      _db = db;
+    }
+
     [HttpGet("/")]
     public ActionResult Index()
     {
-      return View();
+      Category[] cats = _db.Categories.ToArray();
+      Item[] items = _db.Items.ToArray();
+      Dictionary<string, object[]> model = new Dictionary<string, object[]>();
+      model.Add("categories", cats);
+      model.Add("items", items);
+      return View(model);
+      // also valid:
+      // List<Category> cats = _db.Categories.ToList();
+      // List<Item> items = _db.Items.ToList();
+      // ViewBag.categories = cats;
+      // ViewBag.items = items;
+      // return View();
     }
     [Route("/favorite_photos")]
     public ActionResult FavoritePhotos()
